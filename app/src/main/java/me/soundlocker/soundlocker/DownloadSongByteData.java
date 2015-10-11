e1f457f4fd8dc86d63dcc491852944a76c6dc1fe
theworldbright <kent@kentshikama.com>
2015-10-10 17:30:55 -0700

    Use HttpsURLConnection for Spotify previews

--- app/src/main/java/me/soundlocker/soundlocker/DownloadSongByteData.java
+++ app/src/main/java/me/soundlocker/soundlocker/DownloadSongByteData.java
@@ -3,22 +3,24 @@ package me.soundlocker.soundlocker;
 import android.os.AsyncTask;
 
 import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class DownloadSongByteData extends AsyncTask<URL, Integer, Byte[]> {
 
     @Override
     protected Byte[] doInBackground(URL... params) {
         URL url = params[0];
-        HttpURLConnection urlConnection = null;
+        HttpsURLConnection urlConnection = null;
         try {
-            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection = (HttpsURLConnection) url.openConnection();
             InputStream fileInputStream = new BufferedInputStream(urlConnection.getInputStream());
             byte[] result = inputStreamToByteArray(fileInputStream);
             return ArrayUtils.toObject(result);
