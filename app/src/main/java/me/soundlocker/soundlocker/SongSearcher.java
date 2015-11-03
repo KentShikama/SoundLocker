7749dba689fe959a3b2fa21a01512e390443ac49
theworldbright <kent@kentshikama.com>
2015-11-03 12:44:21 -0800

    Move ItemContentReader to inner class of SongSearcher

--- app/src/main/java/me/soundlocker/soundlocker/SongSearcher.java
+++ app/src/main/java/me/soundlocker/soundlocker/SongSearcher.java
@@ -103,4 +103,84 @@ class SongSearcher extends AsyncTask<String, Integer, ArrayList<ImmutablePair<UR
         }
         return url;
     }
+
+    /**
+     * Takes in a JSONReader for a single "song" item object
+     * and allows access to the item's preview URL and album image URL
+     */
+    class ItemContentReader {
+        private JsonReader reader;
+        private URL previewUrl;
+        private URL imageUrl;
+
+        ItemContentReader(JsonReader reader) throws IOException {
+            this.reader = reader;
+            readItemContent();
+        }
+
+        URL getPreviewUrl() {
+            return previewUrl;
+        }
+
+        URL getImageUrl() {
+            return imageUrl;
+        }
+
+        private void readItemContent() throws IOException {
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String symbol = reader.nextName();
+                if (symbol.equals("preview_url")) {
+                    String previewUrlString = reader.nextString();
+                    previewUrl = new URL(previewUrlString);
+                } else if (symbol.equals("album")) {
+                    readAlbum();
+                } else {
+                    reader.skipValue();
+                }
+            }
+            reader.endObject();
+        }
+
+        private void readAlbum() throws IOException {
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String name = reader.nextName();
+                if (name.equals("images")) {
+                    readImages();
+                } else {
+                    reader.skipValue();
+                }
+            }
+            reader.endObject();
+        }
+
+        private void readImages() throws IOException {
+            reader.beginArray();
+            while (reader.hasNext()) {
+                readImage();
+            }
+            reader.endArray();
+        }
+
+        private void readImage() throws IOException {
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String imageSymbol = reader.nextName();
+                if (imageSymbol.equals("url")) {
+                    String imageUrlString = reader.nextString();
+                    assignImageURLIf640x640(imageUrlString);
+                } else {
+                    reader.skipValue();
+                }
+            }
+            reader.endObject();
+        }
+
+        private void assignImageURLIf640x640(String imageUrlString) throws MalformedURLException {
+            if (imageUrl == null) {
+                imageUrl = new URL(imageUrlString);
+            }
+        }
+    }
 }
