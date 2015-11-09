479e0cc9ecc85922db6fc44b592d400501932fe3
Sam Warren <samuel.l.warren@gmail.com>
2015-11-08 22:41:03 -0800

    make webview open user input

--- app/src/main/java/me/soundlocker/soundlocker/WebViewer.java
+++ app/src/main/java/me/soundlocker/soundlocker/WebViewer.java
@@ -1,22 +1,55 @@
 package me.soundlocker.soundlocker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
+
+import java.util.ArrayList;
 
 /**
  * Created by SamW on 11/2/2015.
  */
 public class WebViewer extends Activity{
-
+    private ArrayList<String> webEndings;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        webEndings = new ArrayList<String>();
+        populateWebEndings(webEndings);
+        Intent intent = getIntent();
+        String website = intent.getStringExtra("website");
+
+        String url = makeUrl(website);
         WebView webview = new WebView(this);
-        webview.loadUrl("http://facebook.com/");
+        webview.loadUrl(url);
 
         setContentView(webview);
     }
 
+    private void populateWebEndings(ArrayList<String> aL) {
+        aL.add(".org");
+        aL.add(".com");
+        aL.add(".net");
+        aL.add(".gov");
+        aL.add(".edu");
+    }
+
+    private String makeUrl (String siteName){
+        if (!siteName.contains("http://")){
+            siteName = "http://" + siteName;
+        }
+
+
+
+        for(int i=0; i<webEndings.size(); i++){
+            if(siteName.contains(webEndings.get(i))){
+                return siteName;
+            }
+        }
+        String appendedWebSite = siteName+".com";
+        return appendedWebSite;
+
+    }
     //doesn't work yet 
     public void onPageFinished(WebView view, String url) {
 //        view.loadUrl("javascript:document.getElementsByName('school')[0].value = 'schoolname'");
